4bb904f4be5acd09b697b52fa70c573f3654cfc3
Tony Wilhelm <anthony.wilhelm@moserit.com>
2023-11-22 11:17:46 -0500

    SQL Sat 1057

---
+++ SQL Sat 1057 - OR & SW WA/Intro to Powershell/Demos/Demo1-Commenting.ps1
@@ -0,0 +1,18 @@
+Clear-Host # clears the screen 
+cls
+# using the hash mark will comment every thing after it in that line
+# just like the -- comment in T-SQL
+
+#  <#...#> Will let you comment multiple lines and/or parts of a given line
+# just like the /*...*/ comment in T-SQL
+
+<# 
+   this is a 
+   comment spanning
+   multiple lines   
+#>
+
+#region Sections of your code
+
+
+#endregion
