3c3fb4f6fb12cfe3686ae2c5986772f182422556
Tony Wilhelm <Tonywsql@wilhelm-tech.com>
2024-06-18 13:13:52 -0400

    Initial upload for Albany

---
+++ SQL Sat 1083 - Albany/Intro to Powershell/Demos/Demo2-Variables.ps1
@@ -0,0 +1,85 @@
+#region Untyped
+    Clear-Host
+    $dateString = '04/15/2023 10:45'
+    $dateString
+    $dateString.GetType()
+
+    $instanceString = 'INLAP-WKS1100\SQL2022'
+    $instanceString
+#endregion
+Import-Module dbatools  
+#region Typed
+    Clear-Host
+    [datetime]$dateDT = '04/15/2023 10:45'
+    $dateDT
+    $dateDT.GetType()
+
+    $dateDT2 = [datetime]'04/15/2023 10:45'
+    $dateDT2
+    $dateDT2.GetType()
+    
+    $datedt.AddDays(1)
+    $dateDT.DayOfWeek
+
+    [DbaInstance]$instanceDBA = 'INLAP-WKS1100\SQL2022'
+    $instanceDBA
+    $instancedba.GetType()
+#endregion
+
+#region Custom Objects
+    Clear-Host
+    $customObj = 
+        [PSCustomObject][ordered]@{
+            Name = 'This is the object Name'
+            Value = 123
+            Date = Get-Date
+        }
+    $customObj
+    $customObj.GetType()    
+    $customObj.Name
+    $customObj.date 
+        
+    # these values are read & write
+    $customObj.Name = "Updated Name"
+    $customObj
+
+    # adding new properties
+    $customObj.NewProperty = "Trying to add a new property"
+
+    #region 
+        Add-Member -InputObject $customObj -Name 'NewProperty' -MemberType NoteProperty -Value "Added a new property"
+        $customObj
+    #endregion    
+
+#endregion
+
+#region Arrays
+    Clear-Host
+    $databaseName = @('master', 'model') #declaring the array with 2 items
+    $databaseName += 'msdb'
+    $databaseName
+    $databaseName[0]
+    $databaseName.GetType()
+#endregion
+
+#region Hashtables
+    Clear-Host
+    $hashTable = @{
+
+        Name = 'This is a hashtable'
+        Value = 123
+        Date = Get-Date
+    }
+    $hashTable.GetType()
+    $hashTable.Date    
+#endregion
+
+#region Mixing it up
+    Clear-Host
+    $hashTable.CustomObj = $customObj
+
+    $hashTable
+    $customObj.Date = Get-Date
+    
+    $hashTable
+#endregion 
---
+++ SQL Sat 1083 - Albany/Intro to Powershell/Demos/Demo3-Modules.ps1
@@ -0,0 +1,47 @@
+Clear-Host # clears the screen
+
+#region these require internet connectivity (for the PSGallery) 
+    Install-Module -Name dbatools -Scope CurrentUser 
+    <# scope options
+        AllUsers - This will install the module for all users, requires an elevated command prompt
+            Default for Powershell < 6.0
+        CurrentUser - This will install the module for only the current user
+            Default for Powershell 6.0+, unless it's an elevated session
+    #>
+
+    Uninstall-Module -Name dbatools 
+    Update-Module -Name dbatools  
+
+    Find-Module -Name dbatools -
+#endregion
+
+#region These can be ran locally
+    
+    # from Microsoft.PowerShell.Core module    
+        # List all the modules loaded in memory
+        Get-Module
+        
+        # lists the version(s) of the named module in memory
+        Get-Module -Name dbatools 
+
+        # Lists the installed versions of the name modules
+        Get-Module -Name dbatools -ListAvailable 
+    
+    # Similar to Get-Module, but from the PowerShellGet module
+    Get-InstalledModule -Name dbatools -AllVersions    
+
+    # List all the commands in a given module
+    Get-Command -Module dbatools
+
+    # Load the module into memory
+    Import-Module -Name dbatools 
+    
+    # Removes a module from memory
+    Remove-Module -Name dbatools
+    
+    # getting help on an command
+    Get-Help -Name Get-DbaRegisteredServer -ShowWindow
+    Get-Help -Name Install-Module -ShowWindow
+
+   
+#endregion
